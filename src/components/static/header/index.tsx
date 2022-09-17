cdaf4ef9a5102715cd7db8819b9799bf1c742b60
Dima <dimafilin6@icloud.com>
2022-09-17 22:27:15 +0700

    header fixes

--- src/components/static/header/index.tsx
+++ src/components/static/header/index.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useMemo, useRef, useState} from 'react';
 import './style.css';
 import logo from './assets/dm_logo.png'
 import {HeaderMenuElement} from "../../../model/header-menu-element";
@@ -7,8 +7,10 @@ import sidebarIcon from './assets/sidebar.png'
 import {useAuth} from "../../../model/auth/firebase/auth";
 
 const Header = (props: { menuElements: HeaderMenuElement[] }) => {
-    const [showSidebar, setShowSidebar] = useState(false)
+    const [sidebarState, setSidebarState] = useState<"closed" | "opened">("closed")
     const {currentUser} = useAuth()
+    const sidebarRef = useRef<HTMLDivElement>(null)
+    console.log(sidebarState)
     const buildHeaderElements = useMemo(() => {
         if(props.menuElements) {
             return props.menuElements
@@ -26,7 +28,6 @@ const Header = (props: { menuElements: HeaderMenuElement[] }) => {
         }
 
     }, [props.menuElements, currentUser])
-
     const buildHeaderSidebarElements = useMemo(() => {
         if (props.menuElements)
             return props.menuElements
@@ -37,7 +38,7 @@ const Header = (props: { menuElements: HeaderMenuElement[] }) => {
                         className='nav_link'
                         to={menuElement.menuRoute}>
                         <li className='header__sidebar__element'
-                            key={menuElement.menuName} onClick={() => setShowSidebar(false)}>{menuElement.menuName}</li></NavLink>
+                            key={menuElement.menuName} onClick={() => setSidebarState("closed")}>{menuElement.menuName}</li></NavLink>
         })
     }, [props.menuElements, currentUser])
 
@@ -47,8 +48,16 @@ const Header = (props: { menuElements: HeaderMenuElement[] }) => {
             {buildHeaderElements}
             <menu className='header__sidebar'>
                 <img className='header__sidebar_icon' src={sidebarIcon} alt={'Меню'} height={'32px'}
-                     onClick={() => setShowSidebar(!showSidebar)}/>
-                <div className={"header__sidebar__menu" + (showSidebar ? " expanded" : " collapsed")}>
+                     onClick={() => {
+                         if(sidebarState === "closed") {
+                             if(sidebarRef.current) sidebarRef.current.style.display = "block"
+                             setTimeout(() => setSidebarState("opened"), 1)
+                         }
+                         if(sidebarState === "opened") setSidebarState("closed")
+                     }}/>
+                <div ref={sidebarRef} style={{display: "none"}} className={"header__sidebar__menu"} data-state={sidebarState} onTransitionEnd={() => {
+                    if(sidebarState === "closed" && sidebarRef.current) sidebarRef.current.style.display = "none"
+                }}>
                     {buildHeaderSidebarElements}
                 </div>
             </menu>
