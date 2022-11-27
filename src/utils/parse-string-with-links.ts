cef492fa9249e7508cbce2501256ec715799b4f9
Dima <dimafilin6@icloud.com>
2022-11-27 11:17:47 +0700

    Сокращение ссылки

--- src/utils/parse-string-with-links.ts
+++ src/utils/parse-string-with-links.ts
@@ -1,7 +1,13 @@
 
 export function parseStringWithLinks(str: string) {
     const regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
+    let link = str
+    const wowheadPossibleItem = /item=[0-9]+\//
+    if(wowheadPossibleItem.test(str)) {
+        const exec = wowheadPossibleItem.exec(str)!
+        link = str.substring(0, exec.index + exec[0].length)
+    }
     return str.replace(regex, (old) => {
-        return `<a class='generated-link' href="${old}" target='_blank' rel='norefferer'>${old}</a>`
+        return `<a class='generated-link' href="${old}" target='_blank' rel='norefferer'>${link}</a>`
     })
 }
