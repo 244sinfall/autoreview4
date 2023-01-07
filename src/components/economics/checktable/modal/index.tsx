b6d6994466f57697034a4e084bd75519daf26061
Dmitry Filin <dimafilin6@icloud.com>
2023-01-07 17:46:20 +0700

    ui fixes

--- src/components/economics/checktable/modal/index.tsx
+++ src/components/economics/checktable/modal/index.tsx
@@ -2,7 +2,7 @@ import React, {useMemo} from "react";
 import ModalTitle from "../../../../components/common/modal-title";
 import CheckModalCopyOption from "./modal-field";
 import {Check} from "../../../../model/economics/checks/check";
-
+import './styles.css'
 
 function CheckTableModal(props: {check: Check, closeHandler: () => void}) {
     const rejectCommand = `.check return ${props.check.check.id}`
@@ -13,18 +13,16 @@ function CheckTableModal(props: {check: Check, closeHandler: () => void}) {
         return ""
     }, [props.check])
     return (
-        <ModalTitle title="Макросы для чека" closeCallback={props.closeHandler}>
-            <div className="CheckExecutor-content">
-                {props.check.check.status === "Отказан" && <p>Этот чек отказан. Его невозможно изменить. Игроку необходимо отправить новый чек.</p>}
-                {props.check.check.status === "Закрыт" && <CheckModalCopyOption title="Переоткрыть чек" command={openCommand}/>}
-                {props.check.check.status === "Ожидает" &&
-                  <div className="CheckExecutor-fields">
-                    <CheckModalCopyOption title={"Закрыть чек"} command={closeCommand}/>
-                    <CheckModalCopyOption title={"Отказать чек"} command={rejectCommand}/>
-                  </div>
-                }
-                {reforge && <CheckModalCopyOption title={"Перековка"} command={reforge}/>}
-            </div>
+        <ModalTitle className="check-info-modal" title="Макросы для чека" closeCallback={props.closeHandler}>
+            {props.check.check.status === "Отказан" && <p>Этот чек отказан. Его невозможно изменить. Игроку необходимо отправить новый чек.</p>}
+            {props.check.check.status === "Закрыт" && <CheckModalCopyOption title="Переоткрыть чек" command={openCommand}/>}
+            {props.check.check.status === "Ожидает" &&
+              <>
+                <CheckModalCopyOption title={"Закрыть чек"} command={closeCommand}/>
+                <CheckModalCopyOption title={"Отказать чек"} command={rejectCommand}/>
+              </>
+            }
+            {reforge && <CheckModalCopyOption title={"Перековка"} command={reforge}/>}
         </ModalTitle>
     )
 }
--- src/components/economics/checktable/modal/modal-field/index.tsx
+++ src/components/economics/checktable/modal/modal-field/index.tsx
@@ -2,21 +2,22 @@ import React, {useState} from "react";
 import Field from "../../../../common/field";
 import TextInput from "../../../../common/text-input";
 import ActionButton from "../../../../common/action-button";
+import './styles.css'
 
 function CheckModalCopyOption(props: {title: string, command: string}) {
 
     const [copied, setCopied] = useState(false)
 
     return (
-        <div className="CheckExecutor-field">
+        <div className="check-info-modal-field">
             <Field title={props.title}>
                 <TextInput maxLength={128} defaultValue={props.command}/>
             </Field>
-            <ActionButton title={copied ? "Скопировано" : "Скопировать команду"} onClick={async() => {
+            <span className="check-info-modal-field-button"><ActionButton title={copied ? "Скопировано" : "Скопировать команду"} onClick={async() => {
                 await navigator.clipboard.writeText(props.command)
                 setCopied(true)
                 return setTimeout(() => setCopied(false), 1000)
-            }}/>
+            }}/></span>
         </div>
     )
 
